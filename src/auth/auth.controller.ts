c90a28c12737baeaedb92dff7d649cc387689c66
Jean <jean.carlosmolossi@gmail.com>
2022-10-16 15:39:32 -0300

    lint: passed lint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,3 @@
-import { HttpStatus } from '@/shared/domain/http-status';
-import { User } from '@/users/domain';
 import {
 	Controller,
 	Get,
@@ -10,6 +8,9 @@ import {
 } from '@nestjs/common';
 import { ApiBasicAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 import { Request as eRequest, Response as eResponse } from 'express';
+import { HttpStatus } from '@/shared/domain/http-status';
+import { addMinutes } from '@/shared/helpers/date-math';
+import { User } from '@/users/domain';
 import { Credentials } from './adapter/credentials';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './infra/guards/jwt-auth.guard';
@@ -27,7 +28,9 @@ export class AuthController {
 		const user = request.user as User;
 		const session = await this.authService.login(user);
 
-		response.cookie('oauth', session.oauth);
+		response.cookie('oauth', session.oauth, {
+			expires: addMinutes(new Date(), 15)
+		});
 		response.setHeader('oauth', session.oauth);
 
 		return response.status(HttpStatus.CREATED).send();
--- src/shared/helpers/capitalize.test.ts
+++ src/shared/helpers/capitalize.test.ts
@@ -1,19 +1,19 @@
-import { capitalize, ucFirst } from "./capitalize"
+import { capitalize, ucFirst } from './capitalize';
 
-describe("Helpers > capitalize", function() {
-	test("should upper case first char", function () {
-		const text = "not capitalized"
-		expect(ucFirst(text)).toBe("Not capitalized")
-	})
+describe('Helpers > capitalize', function () {
+	test('should upper case first char', function () {
+		const text = 'not capitalized';
+		expect(ucFirst(text)).toBe('Not capitalized');
+	});
 
-	test("should upper case all first chars", function() {
-		let text = "not capitalized"
-		expect(capitalize(text)).toBe("Not Capitalized")
-		text = "not-capitalized"
-		expect(capitalize(text)).toBe("Not Capitalized")
-		text = "not_capitalized"
-		expect(capitalize(text)).toBe("Not Capitalized")
-		text = "not  -- _ _ capitalized"
-		expect(capitalize(text)).toBe("Not Capitalized")
-	})
-})
+	test('should upper case all first chars', function () {
+		let text = 'not capitalized';
+		expect(capitalize(text)).toBe('Not Capitalized');
+		text = 'not-capitalized';
+		expect(capitalize(text)).toBe('Not Capitalized');
+		text = 'not_capitalized';
+		expect(capitalize(text)).toBe('Not Capitalized');
+		text = 'not  -- _ _ capitalized';
+		expect(capitalize(text)).toBe('Not Capitalized');
+	});
+});
