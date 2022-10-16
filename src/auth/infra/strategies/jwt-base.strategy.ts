5448f0e8515cbf0b30f998fa3ba77c6340c9277c
Jean <jean.carlosmolossi@gmail.com>
2022-10-15 23:58:01 -0300

    feat: custom jwt strategy implemented

---
+++ src/auth/infra/strategies/jwt-base.strategy.ts
@@ -0,0 +1,44 @@
+import { User } from '@/users/domain';
+import { Request } from 'express';
+import { Strategy } from 'passport-strategy';
+
+type Extractor = (request: Request) => string;
+
+interface Options {
+	jwtFromRequest: Extractor;
+	ignoreExpiration: boolean;
+	secretOrKey: string;
+}
+
+export class JwtStrategyBase extends Strategy {
+	name: string = 'jwt';
+
+	constructor(
+		private readonly options: Options,
+		private readonly _verify: (
+			basic: string,
+			done: (t: any) => any
+		) => Promise<User>
+	) {
+		super();
+	}
+
+	async authenticate(request: Request, _options?: any) {
+		const self = this;
+		const token = this.options.jwtFromRequest(request);
+
+		if (!token) return this.fail(new Error('No auth token'), 401);
+
+		try {
+			const done = (user: User, info?: any) => {
+				request.user = user;
+				self.success(user, info);
+			};
+
+			await this._verify(token, done);
+		} catch (e) {
+			console.log('fail', e.message);
+			return self.fail(e, e.status);
+		}
+	}
+}
