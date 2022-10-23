3e0705f9dd4409c55c22c0ef57d5dd0e34b67f11
Jean <jean.carlosmolossi@gmail.com>
2022-10-23 14:08:26 -0300

    docs(swagger): added default value to docs

--- src/shared/infra/adapters/pagination.ts
+++ src/shared/infra/adapters/pagination.ts
@@ -2,11 +2,11 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 
 export class Pagination {
-	@ApiPropertyOptional({ example: '1' })
+	@ApiPropertyOptional({ example: '1', default: '1' })
 	@Type(() => Number)
 	page?: number;
 
-	@ApiPropertyOptional({ example: '15' })
+	@ApiPropertyOptional({ example: '15', default: '15' })
 	@Type(() => Number)
 	per_page?: number;
 }
