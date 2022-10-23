ab36e5959c13d4e0a69f1aeb8c96c1eb63eeeb44
Jean <jean.carlosmolossi@gmail.com>
2022-10-23 14:11:36 -0300

    feat(category_module): added providers to factory

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -4,7 +4,9 @@ import { CategoriesController } from './categories.controller';
 import { CategoryModel } from './infra/repositories/category.entity';
 import { CreateCategoryRepository } from './infra/repositories/create.repository';
 import { FindOneCategoryRepository } from './infra/repositories/find-one.repository';
+import { FindCategoryRepository } from './infra/repositories/find.repository';
 import { CreateCategoryService } from './infra/services/create.service';
+import { FindMyCategoriesService } from './infra/services/find.service';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([CategoryModel])],
@@ -13,8 +15,10 @@ import { CreateCategoryService } from './infra/services/create.service';
 		// repositories
 		CreateCategoryRepository,
 		FindOneCategoryRepository,
+		FindCategoryRepository,
 		// services
-		CreateCategoryService
+		CreateCategoryService,
+		FindMyCategoriesService
 	]
 })
 export class CategoriesModule {}
