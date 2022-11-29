5df735c8a2e6fdf1115b79bcabd6b2eb0557658e
Jean <jean.carlosmolossi@gmail.com>
2022-11-28 22:03:17 -0300

    fix: wrong import

--- src/transactions/infra/repositories/transaction.factory.ts
+++ src/transactions/infra/repositories/transaction.factory.ts
@@ -1,18 +1,18 @@
-import { Transactions } from "dist/transactions/domain/transaction";
-import { setSeederFactory } from "typeorm-extension";
-import { TransactionModel } from "./transactions.entity";
+import { setSeederFactory } from 'typeorm-extension';
+import { Transactions } from '@/transactions/domain';
+import { TransactionModel } from './transactions.entity';
 
-export default setSeederFactory(TransactionModel, (faker) => {
-	const transaction = new TransactionModel()
+export default setSeederFactory(TransactionModel, faker => {
+	const transaction = new TransactionModel();
 
-	transaction.reference = faker.finance.transactionDescription()
+	transaction.reference = faker.finance.transactionDescription();
 	transaction.type = faker.helpers.arrayElement([
 		Transactions.Type.EXPENSE,
 		Transactions.Type.EXPENSE,
 		Transactions.Type.EXPENSE,
-		Transactions.Type.INCOME,
-	])
-	transaction.value = +faker.random.numeric(5, { allowLeadingZeros: true })
+		Transactions.Type.INCOME
+	]);
+	transaction.value = +faker.random.numeric(5, { allowLeadingZeros: true });
 
-	return transaction
-})
+	return transaction;
+});
